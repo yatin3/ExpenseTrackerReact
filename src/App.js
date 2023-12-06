1cab948721703eb70ab7364ae20a4a2ed518380b
yatin goyal <yatingoyal31@gmail.com>
2023-12-06 17:07:32 +0530

    dynamic data passed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <h2>let's get started</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
+  const Expenses = [
+    { id: "e1", title: "furniture", amount: 20, date: new Date(2023, 3, 21) },
+    { id: "e2", title: "accessory", amount: 30, date: new Date(2023, 3, 22) },
+    { id: "e3", title: "grocery", amount: 40, date: new Date(2023, 3, 23) },
+    { id: "e4", title: "chocolatey", amount: 60, date: new Date(2023, 3, 24) },
+  ];
+//   return (
+//     <div>
+//       <h2>let's get started</h2>
+//       <ExpenseItem
+//         title={Expenses[0].title}
+//         amount={Expenses[0].amount}
+//         date={Expenses[0].date}
+//       ></ExpenseItem>
+//       <ExpenseItem
+//         title={Expenses[1].title}
+//         amount={Expenses[1].amount}
+//         date={Expenses[1].date}
+//       ></ExpenseItem>
+//       <ExpenseItem
+//         title={Expenses[2].title}
+//         amount={Expenses[2].amount}
+//         date={Expenses[2].date}
+//       ></ExpenseItem>
+//       <ExpenseItem
+//         title={Expenses[3].title}
+//         amount={Expenses[3].amount}
+//         date={Expenses[3].date}
+//       ></ExpenseItem>
+//     </div>
+//   );
+// }
+
+
+return (
+  <div>
+    <h2>Let's get started</h2>
+    {Expenses.map((element) => (
+      <ExpenseItem
+        key={element.id}  // Don't forget to add a unique key prop when rendering a list of components
+        title={element.title}
+        amount={element.amount}
+        date={element.date}
+      />
+    ))}
+  </div>
+);
+
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,31 +1,15 @@
-// function ExpenseItem() {
-//   return (
-//     <div>
-//       <div>may 23 2023</div>
-//       <div>
-//         <h2>linked jon seeker</h2>
-//         <div>$40</div>
-//       </div>
-//     </div>
-//   );
-// }
+import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     return (
-      <div>
-        <h1>Expense Items</h1>
-        <div>
-          <h2>Food</h2>
-          <div>rs 40</div>
-        </div>
-        <div>
-          <h2>Petrol</h2>
-          <div>rs 100</div>
-        </div>
-        <div>
-          <h2>Movies</h2>
-          <div>rs 200</div>
-        </div>
+      <div className='expense-item'>
+         <div>{props.date.toISOString()}</div>
+         <div className='expense-item__description'>
+            <h2>{props.title}</h2>
+         </div>
+         <div className='expense-item__price'>
+           ${props.amount}
+         </div>
 
       </div>
     );
