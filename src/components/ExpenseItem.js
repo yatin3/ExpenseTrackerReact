f8cf5821ec2a172e0edb8cddf3b7ac8019fa6f0c
yatin goyal <yatingoyal31@gmail.com>
2023-12-06 15:06:45 +0530

    added baisc expenseitem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,35 @@
+// function ExpenseItem() {
+//   return (
+//     <div>
+//       <div>may 23 2023</div>
+//       <div>
+//         <h2>linked jon seeker</h2>
+//         <div>$40</div>
+//       </div>
+//     </div>
+//   );
+// }
+
+function ExpenseItem() {
+    return (
+      <div>
+        <h1>Expense Items</h1>
+        <div>
+          <h2>Food</h2>
+          <div>rs 40</div>
+        </div>
+        <div>
+          <h2>Petrol</h2>
+          <div>rs 100</div>
+        </div>
+        <div>
+          <h2>Movies</h2>
+          <div>rs 200</div>
+        </div>
+
+      </div>
+    );
+  }
+  
+
+export default ExpenseItem;
