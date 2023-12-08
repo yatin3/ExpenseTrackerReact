47e7c7bf4e8df708b1e3da551f104eea8580bba6
yatin goyal <yatingoyal31@gmail.com>
2023-12-08 14:40:10 +0530

    usestate basic functionality

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,18 +1,31 @@
+import React,{useState} from "react";
 import "./ExpenseItem.css";
 
 const ExpenseDetails = (props) => {
+  
+  const [title,setTitle] = useState(props.title);
+  const [amount,setAmount] = useState(props.amount)
+
  const clickHandler = () => {
+  setTitle('updated')
   console.log("clicked!!!")
  }
+
+ const amountChangeHandler = () => {
+  setAmount(100)
+  console.log("amount changed!!!")
+ }
+
 
   return (
     <div className="expense-item">
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__price">${amount}</div>
       <div className="expense-item__price">{props.location}</div>
       <button onClick={clickHandler}>Change Title</button>
+      <button onClick={amountChangeHandler}>Update Amount</button>
     </div>
   );
 }
